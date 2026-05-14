eb26e4dad21912819dff8dfc913f3b387a9a6de3
MartWolf23 <martin.jimenezmu@amigo.edu.co>
2026-05-13 23:34:35 -0500

    fix db connection

--- CarlosApi/hotel-app/backend/db.js
+++ CarlosApi/hotel-app/backend/db.js
@@ -1,25 +1,22 @@
 const mysql = require("mysql2");
 
-console.log('=== DEBUG ENV VARS ===');
-console.log('HOST:', process.env.MYSQL_ADDON_HOST);
-console.log('USER:', process.env.MYSQL_ADDON_USER);
-console.log('DB:', process.env.MYSQL_ADDON_DB);
-console.log('======================');
-
 const db = mysql.createConnection({
-  host: process.env.MYSQL_ADDON_HOST,
-  user: process.env.MYSQL_ADDON_USER,
-  password: process.env.MYSQL_ADDON_PASSWORD,
-  database: process.env.MYSQL_ADDON_DB,
-  port: process.env.MYSQL_ADDON_PORT
+  host: process.env.DB_HOST,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASS,
+  database: process.env.DB_NAME,
+  port: process.env.DB_PORT || 3306,
+  ssl: {
+    rejectUnauthorized: false
+  }
 });
 
 db.connect((err) => {
   if (err) {
-    console.error("Error DB:", err);
-    return;
+    console.log("Error DB:", err);
+  } else {
+    console.log("Conectado a MySQL");
   }
-  console.log("MySQL conectado a Clever Cloud");
 });
 
 module.exports = db;
